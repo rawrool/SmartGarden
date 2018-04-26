3cfb3724c10911c6b1ad976fa8e58dc639101869
Jose Hernandez <josefhu15@gmail.com>
2018-04-25 19:09:17 -0700

    updated google strategy so that we can link and unlink a user account

--- config/passport.js
+++ config/passport.js
@@ -129,41 +129,72 @@ module.exports = function (passport) {
         clientID: configAuth.googleAuth.clientID,
         clientSecret: configAuth.googleAuth.clientSecret,
         callbackURL: configAuth.googleAuth.callbackURL,
+        passReqToCallback: true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
 
-    },
-        function (token, refreshToken, profile, done) {
+    },  // google will send back the token and profile.
+        function (req, token, refreshToken, profile, done) {
 
-            // make the code asynchronous
-            // User.findOne won't fire until we have all our data back from Google
+            // asynchronous
             process.nextTick(function () {
 
-                // try to find the user based on their google id
-                User.findOne({ 'google.id': profile.id }, function (err, user) {
-                    if (err)
-                        return done(err);
+                // check if the user is already logged in
+                if (!req.user) {
+                    // User.findOne won't fire until we have all our data back from Google.
+                    User.findOne({ 'google.id': profile.id }, function (err, user) {
+                        if (err)
+                            return done(err);
 
-                    if (user) {
+                        // if the user is found, then log them in
+                        if (user) {
 
-                        // if a user is found, log them in
+                            // if there is a user id already but no token (user was linked at one point and then removed)
+                            if (!user.google.token) {
+                                user.google.token = token;
+                                user.google.name = profile.displayName;
+                                user.google.email = profile.emails[0].value; // pull the first email
+
+                                user.save(function (err) {
+                                    if (err)
+                                        throw err;
+                                    return done(null, user);
+                                });
+                            }
+
+                            return done(null, user);
+                        } else {
+                            // if no user was found, with that google id then create a new user.
+                            var newUser = new User();
+
+                            newUser.google.id = profile.id;
+                            newUser.google.token = token;
+                            newUser.google.name = profile.displayName;
+                            newUser.google.email = profile.emails[0].value; // pull the first email
+
+                            newUser.save(function (err) {
+                                if (err)
+                                    throw err;
+                                return done(null, newUser);
+                            });
+                        }
+                    });
+
+                } else {
+                    // user already exists and is logged in, we have to link accounts
+                    var user = req.user; // pull the user out of the session
+
+                    user.google.id = profile.id;
+                    user.google.token = token;
+                    user.google.name = profile.displayName;
+                    user.google.email = profile.emails[0].value; // pull the first email
+
+                    user.save(function (err) {
+                        if (err)
+                            throw err;
                         return done(null, user);
-                    } else {
-                        // if the user isnt in our database, create a new user
-                        var newUser = new User();
+                    });
 
-                        // set all of the relevant information
-                        newUser.google.id = profile.id;
-                        newUser.google.token = token;
-                        newUser.google.name = profile.displayName;
-                        newUser.google.email = profile.emails[0].value; // pull the first email
+                }
 
-                        // save the user
-                        newUser.save(function (err) {
-                            if (err)
-                                throw err;
-                            return done(null, newUser);
-                        });
-                    }
-                });
             });
 
         }));
