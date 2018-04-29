fff7b74aa6e2ff65df7e2aa1d55f2c51fb49d0a4
Jason Shortino <akagopher8@gmail.com>
2018-04-29 15:26:23 -0700

    Deleted unnecessary MyTable.swift file. Added comments to existing files to help readability.

--- SmartGarden/FirstViewController.swift
+++ SmartGarden/FirstViewController.swift
@@ -1,4 +1,3 @@
-//
 //  FirstViewController.swift
 //  SmartGarden
 //
@@ -6,21 +5,35 @@
 //  Copyright © 2018 Jason Shortino. All rights reserved.
 //
 
+//automatically created import
 import UIKit
 
+//  This view controller is for the Garden Scene
 class FirstViewController: UIViewController {
-    @IBOutlet weak var tempImageView: UIImageView!
-    @IBOutlet weak var gifImageView: UIImageView!
-    @IBOutlet weak var details: UIButton!
+    //variable to manipulate the garden title label
+    @IBOutlet weak var gardenTitleLabel: UILabel!
+    
+    //variable to manipulate the temperature label
+    @IBOutlet weak var tempLabel: UILabel!
+    
+    //variable to manipulate the humidity label
+    @IBOutlet weak var humidityLabel: UILabel!
+    
+    //variable to manipulate the sprinkler label
+    @IBOutlet weak var sprinklerLabel: UILabel!
+    
+    //variable linked to the "Garden Details"
     @IBOutlet weak var detailsButton: UIButton!
     
     //Look up SwiftyGif or something to display a GIF File
     override func viewDidLoad() {
         super.viewDidLoad()
-        details.layer.cornerRadius = 10
-        details.clipsToBounds = true
+        //Next two lines rounds out the button to look more appealing
+        detailsButton.layer.cornerRadius = 10
+        detailsButton.clipsToBounds = true
     }
 
+    //automatically created function
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- SmartGarden/MyCell.swift
+++ SmartGarden/MyCell.swift
@@ -6,23 +6,30 @@
 //  Copyright © 2018 Jason Shortino. All rights reserved.
 //
 
+//automatically created with the file
 import UIKit
 
+//class to control the custom cell we created in the storyboard in the secondviewcontroller
 class MyCell: UITableViewCell {
+    
+    //variable to manipulate the temperature label in the cell
     @IBOutlet weak var temp_Label: UILabel!
+    //variable to manipulate the humidity label in the cell
     @IBOutlet weak var hum_Label: UILabel!
+    //variable to manipulate the date label in the cell
     @IBOutlet weak var date_Label: UILabel!
     
-    
+    //automatically created with the file
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
     
+    //code for when the cell is selected/tapped on
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
 
-}
+}//end class MyCell
