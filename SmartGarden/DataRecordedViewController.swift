4bd0fc23a2deb7801e12b8873181150e4d53b720
Jason Shortino <akagopher8@gmail.com>
2018-04-29 23:29:58 -0700

    Added more view controllers for views that are on the storyboard.

---
+++ SmartGarden/DataRecordedViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DataRecordedViewController.swift
+//  SmartGarden
+//
+//  Created by Jason Shortino on 4/29/18.
+//  Copyright © 2018 Jason Shortino. All rights reserved.
+//
+
+import UIKit
+
+class DataRecordedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
