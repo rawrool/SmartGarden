86b44c548380a43aa0875b883d7b0b4743a3c074
Jason Shortino <akagopher8@gmail.com>
2018-09-16 21:16:55 -0700

    Added the login view and garden selects views. They are very basic and don't do anything yet

---
+++ SmartGarden/LoginViewController.swift
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.swift
+//  SmartGarden
+//
+//  Created by Jason Shortino on 9/16/18.
+//  Copyright © 2018 Jason Shortino. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Next two lines rounds out the button to look more appealing
+        loginButton.layer.cornerRadius = 10
+        loginButton.clipsToBounds = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
