bbe0016d8fdb8258567cabd7ea268b77ffc67ea4
Jason Shortino <akagopher8@gmail.com>
2018-09-25 15:42:00 -0700

    Getting the token from the server now

--- SmartGarden/LoginViewController.swift
+++ SmartGarden/LoginViewController.swift
@@ -8,14 +8,25 @@
 
 import UIKit
 
+struct LoginResponse:Decodable{
+    let success:String
+    let message:String
+    let token:String
+    
+    init(json: [String: Any]){
+        success = json["success"] as? String ?? ""
+        message = json["message"] as? String ?? ""
+        token = json["token"] as? String ?? ""
+    }
+}
+
 class LoginViewController: UIViewController {
     @IBOutlet weak var usernameText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     
     @IBAction func loginButtonTapped(_ sender: Any) {
-        
-        //does not work yet
+       
         if (attemptLogin( username:usernameText.text!, password:passwordText.text!)){
             print("Done with trying to login and it was successful")
             performSegue(withIdentifier: "loggedIn", sender: sender)
@@ -23,16 +34,6 @@ class LoginViewController: UIViewController {
         else{
             loginErrorAlert()
         }
-        
-        /* Was using to advance to next screen
-         if (usernameText.text == "hi"){
-            //this line will segue to the garden select screen
-            //verify user login before this
-            performSegue(withIdentifier: "loggedIn", sender: sender)
-        }
-        else {
-            loginErrorAlert();
-        }*/
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -71,5 +72,59 @@ class LoginViewController: UIViewController {
         self.present(alertController, animated: true, completion: nil)
     }
     
-    
+    func attemptLogin(username:String, password:String) -> Bool{
+        
+        var successful = false
+        let headers = [
+            "content-type": "application/json"
+        ]
+        
+        let parameters = [
+            "email": "test@gmail.com",//username as Any,
+            "password": "test1234"//password as Any
+            ] as [String : Any]
+        
+        do{
+            let postData = try JSONSerialization.data(withJSONObject: parameters, options: [])
+            let request = NSMutableURLRequest(url: NSURL(string: "http://ec2-18-191-18-131.us-east-2.compute.amazonaws.com/api/authenticate")! as URL,
+                                              cachePolicy: .useProtocolCachePolicy,
+                                              timeoutInterval: 10.0)
+            
+            request.httpMethod = "POST"
+            request.allHTTPHeaderFields = headers
+            request.httpBody = postData as Data
+            
+            let session = URLSession.shared
+            
+            let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+                guard error == nil else {
+                    return
+                }
+                
+                guard let data = data else {
+                    return
+                }
+                
+                do {
+                    
+                    
+                    //create json object from data
+                    if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                        let token = json["token"]!
+                        print(token as Any)
+                        UserDefaults.standard.set(token, forKey: "token")
+                    }
+                } catch let error {
+                    print(error.localizedDescription)
+                }
+            })
+            dataTask.resume()
+        }
+        catch{
+            print("Caught error: ", error)
+        }
+        
+        return successful
+        
+    }
 }
