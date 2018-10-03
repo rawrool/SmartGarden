d43c2dcceeabd4d438bdb829013ed2fbea2fb96b
Jason Shortino <akagopher8@gmail.com>
2018-10-02 18:26:19 -0700

    Working on getting the user's gardens from the server

--- SmartGarden/GardenTableViewController.swift
+++ SmartGarden/GardenTableViewController.swift
@@ -10,11 +10,11 @@ import UIKit
 
 class GardenTableViewController: UITableViewController {
     //sample garden names
-    let gardens = ["Cabbage", "Carrots", "Basil", "Tomatoes"]
+    var gardens = ["Cabbage", "Carrots", "Basil", "Tomatoes"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        getGardens()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -45,6 +45,77 @@ class GardenTableViewController: UITableViewController {
         cell.gardenNameLabel.text = gardens[indexPath.row]
 
         return cell
+    }
+    
+    func getGardens() -> Bool{
+        
+        //set the header for the http request
+        let headers = [
+            "content-type": "application/json"
+        ]
+        //set the parameters for the http request
+        let parameters = [
+            "email": UserDefaults.standard.object(forKey: "username") ?? "No Username",
+            "token": UserDefaults.standard.object(forKey: "token") ?? "No Token"
+            ] as [String : Any]
+        
+        do{
+            let postData = try JSONSerialization.data(withJSONObject: parameters, options: [])
+            let request = NSMutableURLRequest(url: NSURL(string: "http://ec2-18-191-18-131.us-east-2.compute.amazonaws.com/api/gardens")! as URL,
+                                              cachePolicy: .useProtocolCachePolicy,
+                                              timeoutInterval: 10.0)
+            
+            request.httpMethod = "GET"
+            request.allHTTPHeaderFields = headers
+            request.httpBody = postData as Data
+            
+            let session = URLSession.shared
+            
+            let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+                guard error == nil else {
+                    print("error in get")
+                    print(error as Any)
+                    return
+                }
+                
+                guard let data = data else {
+                    return
+                }
+                
+                do {
+                    print("Data from get request")
+                    print(data)
+                    print("Response from get request")
+                    print(response as Any)
+                    print("Error from get request")
+                    print(error as Any)
+                    //create json object from data
+                    /*if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                        guard let success = json["success"] else { return }
+                        print("Success Message:")
+                        print(success)
+                        if success as? Bool ?? false{
+                            guard let token = json["token"] else { return }
+                            let username = UserDefaults.standard.object(forKey: "username") as Any?
+                            print("In dataTask Username: ")
+                            print(username!)
+                            print(token)
+                            //possibly switch to using the username as the key for the stored token
+                            UserDefaults.standard.set(token, forKey: "token")
+                        }
+                    }*/
+                } catch let error {
+                    print(error.localizedDescription)
+                }
+            })
+            
+            dataTask.resume()
+        }
+        catch{
+            print("Caught error: ", error)
+            return false
+        }
+        return true
     }
 
     /*
